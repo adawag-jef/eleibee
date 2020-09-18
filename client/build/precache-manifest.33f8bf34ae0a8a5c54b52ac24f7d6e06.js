aa4974bd0cace0a0daf801ed29227308f907d771
Jeffrey Adawag <jeffrey@tirsolutions.com>
2020-09-18 23:53:56 +0800

    add build

---
+++ client/build/precache-manifest.33f8bf34ae0a8a5c54b52ac24f7d6e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee3b20b2dd833872481ac0bcf8e9fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4eef7dafbe939993656",
+    "url": "/static/css/main.ee0cf02f.chunk.css"
+  },
+  {
+    "revision": "7fe4de99192ab4940eab",
+    "url": "/static/js/2.8034a3b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8034a3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4eef7dafbe939993656",
+    "url": "/static/js/main.854b645b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
